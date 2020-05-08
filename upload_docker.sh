dcc44d4d3bfcba3b5f311e53e4fa9136fa94d05a
Thomas Weibel <thom@ikhono.net>
2020-05-08 16:43:34 +0200

    Upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ikhono/udacity-nd9991-p5:v1
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag ikhono/udacity-nd9991-p5 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
