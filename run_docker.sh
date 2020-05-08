9ad5868c7b45e6fc26f56ec3e614727675429fa9
Thomas Weibel <thom@ikhono.net>
2020-05-08 15:43:56 +0200

    Update script to build and run docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=ikhono/udacity-nd9991-p5:v1 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 ikhono/udacity-nd9991-p5:v1
