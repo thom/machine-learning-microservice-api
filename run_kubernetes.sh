9a9908798ba3852f8e2ddb4aa270447b95306db5
Thomas Weibel <thom@ikhono.net>
2020-05-08 17:04:23 +0200

    Deploy with kubernetes and save output logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ikhono/udacity-nd9991-p5:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-nd9991-p5\
+    --image=$dockerpath\
+    --port=80\
+    --labels app=udacity-nd9991-p5
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-nd9991-p5 8000:80
+kubectl logs udacity-nd9991-p5
